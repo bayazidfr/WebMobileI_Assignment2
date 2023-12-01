0ce5af1f2fe85fe14cb14963e6bd4899f980d785
bayazidfr <144949680+bayazidfr@users.noreply.github.com>
2023-12-01 21:01:02 +0400

    Add API data fetching functionality

--- scripts.js
+++ scripts.js
@@ -7,6 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const modalContent = document.getElementById("modalContent");
   const itemsPerPage = 10;
   let currentPage = 1;
-  let allProducts = [];
+  let allProducts = []; // Store all products globally
 
-})
+  // Fetch data from the API
+  fetch("https://dummyjson.com/products")
+    .then(response => response.json())
+    .then(data => {
+      // Store all products globally
+      allProducts = data.products;
+
+      // Display products initially
+      displayProducts(allProducts);
+
+      // Populate category filter options
+      populateCategoryFilter(allProducts);
+
+      // Render pagination
+      renderPagination(allProducts.length);
+    })
+    .catch(error => console.error("Error fetching data:", error));
