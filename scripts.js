a30a8899fd2779f7305517f8fb1340d89bf58c25
bayazidfr <144949680+bayazidfr@users.noreply.github.com>
2023-12-02 00:09:44 +0400

    git commit -m "Refactor displayProducts function to clear container, filter products, determine display range, and create product cards"

--- scripts.js
+++ scripts.js
@@ -26,3 +26,43 @@ document.addEventListener("DOMContentLoaded", function () {
       renderPagination(allProducts.length);
     })
     .catch(error => console.error("Error fetching data:", error));
+
+  function displayProducts(products) {
+    // Clear the container before adding new products
+    container.innerHTML = "";
+
+    // Filter products based on search input and category filter
+    const filteredProducts = filterProducts(products);
+
+    // Determine the range of products to display for the current page
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const productsToDisplay = filteredProducts.slice(startIndex, endIndex);
+
+    // Iterate through the filtered products and create product cards
+    productsToDisplay.forEach(product => {
+      const card = document.createElement("div");
+      card.classList.add("product-card");
+
+      const thumbnail = document.createElement("img");
+      thumbnail.src = product.thumbnail;
+      thumbnail.alt = product.title;
+      thumbnail.classList.add("product-thumbnail");
+
+      const details = document.createElement("div");
+      details.classList.add("product-details");
+      details.innerHTML = `
+        <h3>${product.title}</h3>
+        <p>Price: $${product.price}</p>
+        <p>Discount: ${product.discountPercentage}%</p>
+        <p>Category: ${product.category}</p>
+      `;
+
+      card.appendChild(thumbnail);
+      card.appendChild(details);
+
+      card.addEventListener("click", () => openProductModal(product));
+
+      container.appendChild(card);
+    });
+  }
