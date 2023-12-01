78ef927319a3f1caa46796fd2da2d2622f539756
bayazidfr <144949680+bayazidfr@users.noreply.github.com>
2023-12-01 15:02:20 +0400

    Initialize variables

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("product-container");
+  const paginationContainer = document.getElementById("pagination-container");
+  const searchInput = document.getElementById("searchInput");
+  const categoryFilter = document.getElementById("categoryFilter");
+  const productModal = document.getElementById("productModal");
+  const modalContent = document.getElementById("modalContent");
+  const itemsPerPage = 10;
+  let currentPage = 1;
+  let allProducts = [];
+
+})
